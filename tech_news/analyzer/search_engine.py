2d63dc9dbd6e904aecb8f2c9cf47946cb698142b
Sarah <sarahgsb@hotmail.com>
2022-03-11 15:43:05 -0300

    search_engine: requisitos 6, 7, 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,49 @@
+from tech_news.database import search_news
+import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    title_list = []
+    search_title = search_news({"title": {"$regex": title.capitalize()}})
+
+    for titles in search_title:
+        title_list.append((titles["title"], titles["url"]))
+    return title_list
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    date_list = []
+    search_date = search_news({"timestamp": {"$regex": date}})
+
+    try:
+        datetime.date.fromisoformat(date)
+
+        for dates in search_date:
+            date_list.append((dates["title"], dates["url"]))
+        return date_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    source_list = []
+    source_news = search_news({"sources": {"$regex": source, "$options": "i"}})
+
+    for sources in source_news:
+        source_list.append((sources["title"], sources["url"]))
+    return source_list
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    category_list = []
+    category_news = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+
+    for categories in category_news:
+        category_list.append((categories["title"], categories["url"]))
+    return category_list
